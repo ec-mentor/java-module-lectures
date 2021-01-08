baff297d542b9f988a10d08cfe1a2b926c2f8264
Alexander von Franqué <alexander@vonfranque.com>
2021-01-08 11:42:03 +0100

    Initial commit

---
+++ src/academy/everyonecodes/java/week1/set1/exercise1/Main.java
@@ -0,0 +1,8 @@
+package academy.everyonecodes.java.week1.set1.exercise1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
